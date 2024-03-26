defa3f368b792aee64d9d2b4e29beab06d39c81f
unknown <dejanmaric259@gmail.com>
2024-03-26 20:45:12 +0100

    connecting api with front

---
+++ frontend/reactapp/src/Components/Apitest.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios' ; 
+
+
+function Apitest() {
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+      axios.get('http://127.0.0.1:8000//api/hello-world/')
+        .then(response => {
+          setMessage(response.data.message);
+        })
+        .catch(error => {
+            setMessage(error.message)
+          console.log(error);
+        });
+    }, [message]);
+  
+  return (
+    <div>Apitest
+        <p>{message}</p>
+    </div>
+  )
+}
+
+export default Apitest
