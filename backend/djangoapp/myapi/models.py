880b995e239de5948915d96040dbb336a002ac4a
djoleb1 <djordje.brdar2202@gmail.com>
2024-03-27 21:23:38 +0100

    adding .env and User model

--- backend/djangoapp/myapi/models.py
+++ backend/djangoapp/myapi/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+
+    username = models.CharField(max_length=50, unique=True, blank=False)
+    first_name = models.CharField(max_length=50, blank=False)
+    last_name = models.CharField(max_length=50, blank=False)
+    email = models.EmailField(max_length=100, unique=True, blank=False)
+    password = models.CharField(max_length=256)
+    
