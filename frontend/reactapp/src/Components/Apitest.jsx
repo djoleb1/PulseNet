3658bb5c56e22940dd8587b9f270c0caf0c65f7b
unknown <dejanmaric259@gmail.com>
2024-03-28 00:15:23 +0100

    handling register on frontend

--- frontend/reactapp/src/Components/Apitest.jsx
+++ frontend/reactapp/src/Components/Apitest.jsx
@@ -14,10 +14,25 @@ function Apitest() {
             setMessage(error.message)
           console.log(error);
         });
+
+        axios.post('http://127.0.0.1:8000/api/register/', {
+          username: 'deki123123', 
+          first_name: 'deki',  
+          last_name: 'deki', 
+          email: 'deki@example.com', 
+          password: 'deki',
+        })
+        .then(response => {
+          console.log(response.data); // Handle successful registration
+        })
+        .catch(error => {
+          console.error('Registration failed:', error); // Handle registration error
+        });
     }, [message]);
   
   return (
-    <div>Apitest
+    <div>
+      Apitest
         <p>{message}</p>
     </div>
   )
